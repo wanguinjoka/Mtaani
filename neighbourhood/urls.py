c7ec660f5003c007baa033ef5836e6ef04130927
wangui.njoka <wangui.njoka@gmail.com>
2018-10-20 18:12:06 +0300

    displaying detail page

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from .views import KijijiDetailView
 from . import views
 
 urlpatterns=[
     url('^$', views.home, name='home'),
+    url(r'^kijiji/(?P<pk>[0-9]+)/$', KijijiDetailView.as_view(), name='kijiji-detail'),
 ]
--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import DetailView, CreateView,UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Kijiji
 # Create your views here.
 def home(request):
@@ -7,3 +9,6 @@ def home(request):
         'kijijis': Kijiji.objects.all()
     }
     return render(request,'neighbourhood/home.html', context)
+
+class KijijiDetailView(LoginRequiredMixin, DetailView):
+     model = Kijiji
