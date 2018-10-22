ed658c0797cb62ef6cb5e0ce3a550ae0a6bb8bf2
wangui.njoka <wangui.njoka@gmail.com>
2018-10-22 09:38:44 +0300

    working on form for new news post

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import KijijiDetailView
+from .views import KijijiDetailView,NewsCreateView
 from . import views
 
 urlpatterns=[
     url('^$', views.home, name='home'),
     url(r'^kijiji/(?P<pk>[0-9]+)/$', KijijiDetailView.as_view(), name='kijiji-detail'),
+    url(r'^Kijiji/news/', NewsCreateView.as_view(), name='news-create'),
 ]
--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 from django.views.generic import DetailView, CreateView,UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Kijiji
+from .forms import CreateNewsForm
+from .models import Kijiji, News
 # Create your views here.
 def home(request):
     context={
@@ -13,3 +14,26 @@ def home(request):
 
 class KijijiDetailView(LoginRequiredMixin, DetailView):
      model = Kijiji
+
+# @login_required(login_url='/accounts/login/')
+# def new_news(request):
+#     current_user = request.user
+#     if request.method == 'POST':
+#         form = CreateNewsForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             news = form.save(commit=False)
+#             news.author = current_user
+#             news.save()
+#         return redirect('')
+#
+#     else:
+#         form = CreateNewsForm()
+#     return render(request, 'new_article.html', {"form": form})
+
+class NewsCreateView(LoginRequiredMixin, CreateView):
+    model = News
+    fields = ['tag','cation','kijiji']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
